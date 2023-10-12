4fcc7e72f234050dd65773f6acadbf3357b6abaa
Matthew Alvarez <mattalvarez354@yahoo.com>
2023-10-12 19:33:28 -0400

    Updated post test for api using jest and supertest

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -11,9 +11,57 @@ describe("Test GET /launches", () => {
 });
 
 describe("Test POST /launches", () => {
-  test("It should respond with 201 created", async () => {});
+  const completeLaunchData = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+    launchDate: "January 4, 2028",
+  };
 
-  test("It should catch missing required properties", async () => {});
+  const launchDataWithoutDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+  };
 
-  test("It should catch invalid dates", async () => {});
+  test("It should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(responseDate).toBe(requestDate);
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
+
+  test("It should catch missing required properties", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send({
+        mission: "USS Enterprise",
+        rocket: "NCC 1701-D",
+        target: "",
+        launchDate: "January 4, 2028",
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+  });
+
+  test("It should catch invalid dates", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send({
+        mission: "USS Enterprise",
+        rocket: "NCC 1701-D",
+        target: "Kepler-186 f",
+        launchDate: "January 4, 28",
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+  });
 });
